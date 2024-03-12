2f1ba64109ce0e21056d224348ee465ef3458286
Paul Mialane <paul.mialane@telecom-paris.fr>
2024-03-12 14:33:28 +0100

    Autre test logiciel

---
+++ TestLogiciel/blinky/build/zephyr/zephyr.dts.d
@@ -0,0 +1,31 @@
+empty_file.o: \
+ /home/paulm/Documents/TélécomParis/1A/ARTISHOW/zephyr/misc/empty_file.c \
+ /home/paulm/Documents/TélécomParis/1A/ARTISHOW/zephyr/boards/arm/xiao_ble/xiao_ble_sense.dts \
+ /home/paulm/Documents/TélécomParis/1A/ARTISHOW/zephyr/boards/arm/xiao_ble/xiao_ble_common.dtsi \
+ /home/paulm/Documents/TélécomParis/1A/ARTISHOW/zephyr/dts/arm/nordic/nrf52840_qiaa.dtsi \
+ /home/paulm/Documents/TélécomParis/1A/ARTISHOW/zephyr/dts/common/mem.h \
+ /home/paulm/Documents/TélécomParis/1A/ARTISHOW/zephyr/dts/arm/nordic/nrf52840.dtsi \
+ /home/paulm/Documents/TélécomParis/1A/ARTISHOW/zephyr/dts/arm/armv7-m.dtsi \
+ /home/paulm/Documents/TélécomParis/1A/ARTISHOW/zephyr/dts/common/skeleton.dtsi \
+ /home/paulm/Documents/TélécomParis/1A/ARTISHOW/zephyr/dts/common/nordic/nrf_common.dtsi \
+ /home/paulm/Documents/TélécomParis/1A/ARTISHOW/zephyr/include/zephyr/dt-bindings/adc/adc.h \
+ /home/paulm/Documents/TélécomParis/1A/ARTISHOW/zephyr/include/zephyr/dt-bindings/adc/nrf-adc.h \
+ /home/paulm/Documents/TélécomParis/1A/ARTISHOW/zephyr/include/zephyr/dt-bindings/dt-util.h \
+ /home/paulm/Documents/TélécomParis/1A/ARTISHOW/zephyr/include/zephyr/sys/util_macro.h \
+ /home/paulm/Documents/TélécomParis/1A/ARTISHOW/zephyr/include/zephyr/sys/util_internal.h \
+ /home/paulm/Documents/TélécomParis/1A/ARTISHOW/zephyr/include/zephyr/sys/util_loops.h \
+ /home/paulm/Documents/TélécomParis/1A/ARTISHOW/zephyr/include/zephyr/sys/util_listify.h \
+ /home/paulm/Documents/TélécomParis/1A/ARTISHOW/zephyr/include/zephyr/sys/util_internal_is_eq.h \
+ /home/paulm/Documents/TélécomParis/1A/ARTISHOW/zephyr/include/zephyr/sys/util_internal_util_inc.h \
+ /home/paulm/Documents/TélécomParis/1A/ARTISHOW/zephyr/include/zephyr/sys/util_internal_util_dec.h \
+ /home/paulm/Documents/TélécomParis/1A/ARTISHOW/zephyr/include/zephyr/sys/util_internal_util_x2.h \
+ /home/paulm/Documents/TélécomParis/1A/ARTISHOW/zephyr/include/zephyr/dt-bindings/gpio/gpio.h \
+ /home/paulm/Documents/TélécomParis/1A/ARTISHOW/zephyr/include/zephyr/dt-bindings/i2c/i2c.h \
+ /home/paulm/Documents/TélécomParis/1A/ARTISHOW/zephyr/include/zephyr/dt-bindings/input/input-event-codes.h \
+ /home/paulm/Documents/TélécomParis/1A/ARTISHOW/zephyr/include/zephyr/dt-bindings/pinctrl/nrf-pinctrl.h \
+ /home/paulm/Documents/TélécomParis/1A/ARTISHOW/zephyr/include/zephyr/dt-bindings/pwm/pwm.h \
+ /home/paulm/Documents/TélécomParis/1A/ARTISHOW/zephyr/dts/common/freq.h \
+ /home/paulm/Documents/TélécomParis/1A/ARTISHOW/zephyr/dts/arm/nordic/override.dtsi \
+ /home/paulm/Documents/TélécomParis/1A/ARTISHOW/zephyr/boards/arm/xiao_ble/xiao_ble-pinctrl.dtsi \
+ /home/paulm/Documents/TélécomParis/1A/ARTISHOW/zephyr/boards/arm/xiao_ble/seeed_xiao_connector.dtsi \
+ /home/paulm/Documents/TélécomParis/1A/ARTISHOW/zephyr/include/zephyr/dt-bindings/gpio/nordic-nrf-gpio.h

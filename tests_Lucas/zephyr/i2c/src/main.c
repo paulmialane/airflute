0bb80b0250eac93fae871468b3919c7a3cc376c7
Lucas Michel <lucas.michel@telecom-paris.fr>
2024-04-24 18:02:54 +0200

    Controleur de GPIO i2c fonctionnel

--- tests_Lucas/zephyr/i2c/src/main.c
+++ tests_Lucas/zephyr/i2c/src/main.c
@@ -1,19 +1,19 @@
-
-
-
 #include <stdio.h>
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
 
-#define EXT_GPIO_CTRL_1 DT_NODELABEL(i2c1) 
+#define EXT_GPIO_CTRL_1 DT_NODELABEL(pcf8574_1)
 //structure pour contrôler le device
-
-#define ext_gpio_1 DEVICE_DT_GET(EXT_GPIO_CTRL_1)
-//static const struct device * const ext_gpio_1 = DEVICE_DT_GET(EXT_GPIO_CTRL_1);
+static const struct device * const ext_gpio_1 = DEVICE_DT_GET(EXT_GPIO_CTRL_1);
 
 #define SLEEP_TIME_MS 1000
 
 int main(void) { //ça semble important que ça soit un int
+    // pas vraiment besoin de configurer les pins en entrée pour le PCF8574,
+    // il faut juste éviter d'écrire dessus
+    // une variable pour récupérer la valeur du GPIO
+    gpio_port_value_t val_gpio_1; // devrait être de type gpio_port_value_t (était un int)
+
     k_msleep(SLEEP_TIME_MS);
     printk("coucou\n");
     k_msleep(SLEEP_TIME_MS);
@@ -24,15 +24,11 @@ int main(void) { //ça semble important que ça soit un int
     }
     printk("c'est prêt !!\n");
     k_msleep(SLEEP_TIME_MS);
-    
-    
-    // pas vraiment besoin de configurer les pins en entrée pour le PCF8574,
-    // il faut juste éviter d'écrire dessus
-    // une variable pour récupérer la valeur du GPIO 
-    gpio_port_value_t val_gpio_1; // devrait être de type gpio_port_value_t (était un int)
 
-    gpio_port_get(ext_gpio_1, &val_gpio_1); // ou gpio_pin_get(...) si on veut juste une des pins
+    while (1){
+            gpio_port_get(ext_gpio_1, &val_gpio_1); // ou gpio_pin_get(...) si on veut juste une des pins
 
-    printk("--> %02x\n",val_gpio_1); 
-    k_msleep(SLEEP_TIME_MS);
+            printk("--> %02x\n",val_gpio_1);
+            k_msleep(SLEEP_TIME_MS);
+    }
 }

01d4c1798a43e9b65a5b05ff6591c6a49e0ad3a7
Paul Mialane <paul.mialane@telecom-paris.fr>
2024-06-25 13:43:47 +0200

    Ajout de commentaires

--- tests_Justine/test_archi_onoff/src/triggerChange.c
+++ tests_Justine/test_archi_onoff/src/triggerChange.c
@@ -14,26 +14,29 @@ void triggerChangeThread(struct k_fifo* currentlyPlayingFifo, struct k_fifo* but
 
 	while(1){
 		k_msleep(500);
-		printk("still alive");
-		int isBlowing = 0;
+		printk("STILL ALIVE\n");
+
+		// We check if we are blowing
+		int isBlowing;
 		isBlowing = blowingOnOff();
 		printk("Est-ce qu'on souffle : %d\n", isBlowing);
-		if(isBlowing){
 
+		if(isBlowing){
+			// If we are blowing, we get the combination of buttons
 			int oldCombination[8] = {}; // On part du principe qu'il n'y a pas d'ancienne combinaison/
 			int newCombination[8] = {};
-			getCombination(newCombination); // récupère l'état des capteurs
+			getCombination(newCombination);
 
-			printk("capteurs doigts récupérés\n");
+			printk("Capteurs doigts récupérés\n");
 
 
-			if (!k_fifo_is_empty(currentlyPlayingFifo)){ //Si on est déjà en train de jouer une note, on la récupère
-
+			if (!k_fifo_is_empty(currentlyPlayingFifo)){ 
+				 // If we were playing a note, check what are we playing
 				struct note_data *rx_data = k_fifo_peek_head(currentlyPlayingFifo); // attention ! on ne récupère pas les données (ce qui les enlèverait de la file), on regarde juste
 
 				copyArray(oldCombination, rx_data->buttons);
 
-				printk("ancienne combinaison récupérée\n");
+				printk("Ancienne combinaison récupérée\n");
 			}
 
 			if (!isEqual(oldCombination, newCombination)){ 
@@ -44,11 +47,11 @@ void triggerChangeThread(struct k_fifo* currentlyPlayingFifo, struct k_fifo* but
 				combinaison car on n'était pas en train de jouer
 			*/
 
-			    printk("on a changé de note\n");
+			    printk("On a changé de note\n");
 
-				/*arrêter de jouer l'ancienne note*/
+				/*We need to stop playing the current note*/
 				if (!k_fifo_is_empty(currentlyPlayingFifo)){ 
-					
+					// If we were playing a note, we stop playing it
 					struct note_data *rx_data = k_fifo_get(currentlyPlayingFifo, K_FOREVER);
 					rx_data->on = 0;
 					size_t size = sizeof(struct note_data);
@@ -57,16 +60,19 @@ void triggerChangeThread(struct k_fifo* currentlyPlayingFifo, struct k_fifo* but
 
 					memcpy(mem_ptr, rx_data, size);
 
+					// We put the note OFF in the fifo
 					k_fifo_put(noteToPlayFifo, mem_ptr);
+
+					// As we don't need rx_data anymore, we free the memory
 					k_free(rx_data);
 				}
 
-				/*nouvelle combinaison*/
+				// Now that we stopped playing the note, we can play the new one
 
 				struct note_data tx_data = { .buttons = {}, .strength = 127, .note = 0, .on = 1};
-
 				copyArray(tx_data.buttons, newCombination);
 
+				// This SHOULD BE unnecessary
 				size_t size = sizeof(struct note_data);
 				struct note_data *mem_ptr = k_malloc(size);
 				__ASSERT_NO_MSG(mem_ptr != 0);
@@ -75,18 +81,18 @@ void triggerChangeThread(struct k_fifo* currentlyPlayingFifo, struct k_fifo* but
 
 				k_fifo_put(buttonsCombinationFifo, mem_ptr);
 
-				printk("nouvelle combi mise dans la file\n");
+				printk("Nouvelle combi mise dans la file\n");
 
 			}
 
 		}
 
 		else {
-			
+			// If we are not blowing, we stop playing the note
 			if (!k_fifo_is_empty(currentlyPlayingFifo)){
+				// If we were playing a note, we stop playing it
 
-				printk("il faut arrêter de jouer\n");
-
+				// We get the note we are currently playing
 				struct note_data *rx_data = k_fifo_get(currentlyPlayingFifo, K_FOREVER); 
 
 				rx_data->on = 0;
@@ -99,9 +105,12 @@ void triggerChangeThread(struct k_fifo* currentlyPlayingFifo, struct k_fifo* but
 
 				memcpy(mem_ptr, rx_data, size);
 
+				// We put the note OFF in the fifo
 				k_fifo_put(noteToPlayFifo, mem_ptr);
+
+				// As we don't need rx_data anymore, we free the memory
 				k_free(rx_data);
-				printk("on a demandé d'arrêter de jouer l'ancienne note\n");
+				printk("On a demandé d'arrêter de jouer l'ancienne note\n");
 			}
 		}
 	}

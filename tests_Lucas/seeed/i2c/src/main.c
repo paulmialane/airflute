03b3fd7b2d7d0a004fd1d16f401048f34e486f38
Lucas Michel <lucas.michel@telecom-paris.fr>
2024-06-25 11:20:59 +0200

    ajustement binds

--- tests_Lucas/seeed/i2c/src/main.c
+++ tests_Lucas/seeed/i2c/src/main.c
@@ -9,7 +9,11 @@ int main(void) {
     while (1){
             int val_gpio_1 = getKeyMask(); // ou gpio_pin_get(...) si on veut juste une des pins
 
-            printk(" %x", val_gpio_1);
+            printk("\r");
+            for (int i=0; i<8; i++){
+                printk("%d", (val_gpio_1>>i&1));
+
+            }
 
             k_msleep(100);
     }

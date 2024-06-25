0a4cbe8a0eccf713e22811dd6d408d8cb6a14536
Paul Mialane <paul.mialane@telecom-paris.fr>
2024-06-25 13:26:21 +0200

    Ajout de commentaire et modification des messages de debug

--- tests_Justine/test_archi_onoff/src/playNote.c
+++ tests_Justine/test_archi_onoff/src/playNote.c
@@ -10,30 +10,36 @@
 
 void playNoteThread(struct k_fifo* noteToPlayFifo, struct k_fifo* currentlyPlayingFifo){
 	midiInitialize();
-	printk("midi initialisé\n");
+	printk("Midi initialisé\n------------------\n");
 
 	while(1){
+		// We get the note to play out of the fifo
 		struct note_data *rx_data = k_fifo_get(noteToPlayFifo, K_FOREVER);
 
-		printk("note à jouer récupérée\n");
+		printk("Note à jouer récupérée\n");
 
+		// We extract the note, the force of the note, the combination of buttons, and whether the note is on or off
 		int note = rx_data->note;
 		int force = rx_data->strength;
-		int combi[8] = {};
-		copyArray(combi, rx_data->buttons);
+		int combinaison[8] = {};
+		copyArray(combinaison, rx_data->buttons);
 		int on = rx_data->on;
 
+		// As we don't need rx_data anymore, we free the memory
 		k_free(rx_data);
 
 		if (isConnectedOverBLE()){
-			printk("joue une note\n");
+			// If we are connected to a device over BLE, we play the note
+			printk("Je vais jouer une note\n");
 			sendNote(on, note, force);
-			printk("note jouée\n");
+			printk("Note jouée\n");
 
+			// We create a new note_data struct to put in the fifo
 			struct note_data tx_data = {.buttons = {}, .strength = force , .note = note, .on = 1};
 
-			copyArray(tx_data.buttons, combi);
+			copyArray(tx_data.buttons, combinaison);
 			
+			// This SHOULD BE unnecessary
 			size_t size = sizeof(struct note_data);
 			struct note_data *mem_ptr = k_malloc(size);
 			__ASSERT_NO_MSG(mem_ptr != 0);
@@ -42,13 +48,13 @@ void playNoteThread(struct k_fifo* noteToPlayFifo, struct k_fifo* currentlyPlayi
 
 			if (on){
 				k_fifo_put(currentlyPlayingFifo, mem_ptr);
-				printk("on a indiqué dans la file qu'on était en train de jouer\n");
+				printk("On a indiqué dans la file qu'on était en train de jouer\n");
 			}
 
 		}
 		
 		else{
-			printk("on ne joue rien car on n'est pas connecté en Bluetooth...\n");
+			printk("On ne joue rien car on n'est pas connecté en Bluetooth...\n");
 		}
 
 	}
